e0ac519d61b337a8d48a49fc03c86d7d39f1911c
MTG863-web <gpeewee863@yahoo.com>
2019-11-04 11:16:08 -0500

    setting view engine to ejs and creating views folder for user views

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+ app.set('view engine', 'ejs');
+
+ /*
+    ROUTES
+ */
  
+ // GET /
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.render('home.ejs');
+});
  
 app.listen(3000, function(){
     console.log('Hello There!');
